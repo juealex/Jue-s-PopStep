6eea6f30ddf6a020ccb8f0446c37728a65f5c8e7
JueAlexer <myamyahtike766@gmail.com>
2025-01-17 20:59:21 +0630

    ...

---
+++ src/sever.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../public/views'));
+
+app.listen(port, () => {
+    console.log('Server is running on http://localhost:3000');
+});
